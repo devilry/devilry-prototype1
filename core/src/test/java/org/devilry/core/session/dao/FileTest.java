19a2efd0db221aa6bc51d86a32a31688dbe880db
Espen A. Kristiansen <xxespen@gmail.com>
2009-05-14 14:01:12 +0200

    Added forgotten files from my last commit.

---
+++ core/src/test/java/org/devilry/core/session/dao/FileTest.java
@@ -0,0 +1,58 @@
+package org.devilry.core.session.dao;
+
+import javax.naming.NamingException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class FileTest extends AbstractSessionBeanTestHelper {
+
+	FileMetaRemote fileMeta;
+	DeliveryCandidateRemote deliveryCandidate;
+	long fileId;
+
+	@Before
+	public void setUp() throws NamingException {
+		setupEjbContainer();
+		fileMeta = (FileMetaRemote) getRemoteBean(File.class);
+		deliveryCandidate = (DeliveryCandidateRemote) getRemoteBean(DeliveryCandidate.class);
+		deliveryCandidate.init(0);
+		fileId = deliveryCandidate.addFile("test.txt");
+		fileMeta.init(fileId);
+	}
+
+	@After
+	public void tearDown() {
+		destroyEjbContainer();
+	}
+
+	@Test
+	public void getId() {
+		assertEquals(fileId, fileMeta.getId());
+	}
+
+	@Test
+	public void getDeliveryCandidateId() {
+		assertEquals(deliveryCandidate.getId(), fileMeta.getDeliveryCandidateId());
+	}
+
+	@Test
+	public void getFilePath() {
+		assertEquals("test.txt", fileMeta.getFilePath());
+	}
+
+	@Test
+	public void readwrite() {
+		fileMeta.write("a".getBytes());
+		fileMeta.write("b".getBytes());
+		fileMeta.write("c".getBytes());
+		assertEquals("a", new String(fileMeta.read()));
+		assertEquals("b", new String(fileMeta.read()));
+		assertEquals("c", new String(fileMeta.read()));
+
+		fileMeta.resetReadState();
+		assertEquals("a", new String(fileMeta.read()));
+	}
+}
