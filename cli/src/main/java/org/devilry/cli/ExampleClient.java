a3476b0c6209577e814890242c57924b2cb658cd
Espen A. Kristiansen <post@espenak.net>
2009-05-15 18:01:35 +0200

    Made the example client work with an external server.

--- cli/src/main/java/org/devilry/cli/ExampleClient.java
+++ cli/src/main/java/org/devilry/cli/ExampleClient.java
@@ -1,7 +1,70 @@
 package org.devilry.cli;
 
+import org.devilry.core.session.TreeManagerRemote;
+import org.devilry.core.session.TreeManagerImpl;
+import org.devilry.core.session.dao.NodeRemote;
+import org.devilry.core.session.dao.NodeImpl;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
 public class ExampleClient {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws NamingException {
+		new ExampleClient();
+	}
+
+	Context serverConnection;
+
+	public ExampleClient() throws NamingException {
+		serverConnection = getServerConnection();
+
+		// Get the tree mananger bean
+		// same as:
+		// TreeManagerRemote tm = (TreeManagerRemote) serverConnection.lookup("TreeManagerImplRemote");
+		TreeManagerRemote tm = getRemoteBean(TreeManagerImpl.class);
+
+		// Add some nodes
+		tm.addNode("uio", "Universitetet i Oslo");
+		tm.addNode("matnat", "Matematisk...", tm.getNodeIdFromPath("uio"));
+		tm.addNode("ifi", "Institutt for informatikk", tm.getNodeIdFromPath("uio.matnat"));
+
+		// List nodes
+		NodeRemote uio = getRemoteBean(NodeImpl.class);
+		uio.init(tm.getNodeIdFromPath("uio"));
+		for(long facultyId: uio.getChildren()) {
+			NodeRemote faculty = getRemoteBean(NodeImpl.class);
+			faculty.init(facultyId);
+			System.out.println(faculty.getPath());
+		}
+	}
+
+	public Context getServerConnection() throws NamingException {
+		Properties p = new Properties();
+//
+//		// Embed openejb
+//		p.put(Context.INITIAL_CONTEXT_FACTORY,
+//				"org.apache.openejb.client.LocalInitialContextFactory");
+//
+//		// Set bean naming (JNDI) format
+//		p.put("openejb.deploymentId.format",
+//				"{ejbName}{interfaceType.annotationName}");
+//		p.put("openejb.jndiname.format",
+//				"{ejbName}{interfaceType.annotationName}");
+		p.put(Context.INITIAL_CONTEXT_FACTORY,
+			"org.apache.openejb.client.RemoteInitialContextFactory");
+		p.put(Context.PROVIDER_URL, "ejbd://127.0.0.1:4201");
+		p.put("java.naming.security.principal", "jonathan");
+		p.put("java.naming.security.credentials", "secret");
+
+		return new InitialContext(p);
+	}
+
+	@SuppressWarnings("unchecked")
+	protected <E> E getRemoteBean(Class<E> beanImplClass)
+			throws NamingException {
+		return (E) serverConnection.lookup(beanImplClass.getSimpleName() + "Remote");
 	}
 }
