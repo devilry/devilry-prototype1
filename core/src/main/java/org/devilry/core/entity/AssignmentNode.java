2fadd6ba75d239370ea32b1cce73ddbf02f868b8
Bendik <bendikro@gmail.com>
2009-05-31 19:24:16 +0200

    Added deadline Date in Assignment

--- core/src/main/java/org/devilry/core/entity/AssignmentNode.java
+++ core/src/main/java/org/devilry/core/entity/AssignmentNode.java
@@ -1,11 +1,24 @@
 package org.devilry.core.entity;
 
+import java.util.Date;
+
 import javax.persistence.*;
 
 @Entity
 @DiscriminatorValue("AN")
 public class AssignmentNode extends Node {
+	
+	@Temporal(TemporalType.DATE)
+	private Date deadline;
+	
 	public AssignmentNode() {
+	}
 
+	public Date getDeadline() {
+		return this.deadline;
+	}
+
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
 	}
 }
