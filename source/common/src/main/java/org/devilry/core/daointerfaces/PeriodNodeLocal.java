f297543fdb2c4510ca0fc0c4fddfc62a1e3c353f
Bendik <bendikro@gmail.com>
2009-06-02 02:20:28 +0200

    changed long parameter to periodNodeId

--- source/common/src/main/java/org/devilry/core/daointerfaces/PeriodNodeLocal.java
+++ source/common/src/main/java/org/devilry/core/daointerfaces/PeriodNodeLocal.java
@@ -4,10 +4,10 @@ import java.util.Date;
 import java.util.List;
 
 public interface PeriodNodeLocal extends BaseNodeInterface {
-	public void setStartDate(long nodeId, Date start);
-	public Date getStartDate(long nodeId);
-	public void setEndDate(long nodeId, Date end);
-	public Date getEndDate(long nodeId);
+	public void setStartDate(long periodNodeId, Date start);
+	public Date getStartDate(long periodNodeId);
+	public void setEndDate(long periodNodeId, Date end);
+	public Date getEndDate(long periodNodeId);
 	public long create(String name, String displayName, Date start, Date end, long parentId);
-	public List<Long> getAssignments(long parentId);
+	public List<Long> getAssignments(long periodNodeId);
 }
