a15bd060b828291b9a2d573c67c625e4e9efd39c
Espen A. Kristiansen <post@espenak.net>
2009-06-04 07:33:11 +0200

    Rewrote the node entities with an abstract superclass for all nodes.

---
+++ source/core/src/main/java/org/devilry/core/entity/BaseNode.java
@@ -0,0 +1,69 @@
+package org.devilry.core.entity;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToMany;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="nodeType", discriminatorType=DiscriminatorType.STRING)
+@DiscriminatorValue("N")
+public abstract class BaseNode implements java.io.Serializable {
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@Column(name="name")
+	private String name;
+
+	private String displayName;
+
+	@ManyToMany(cascade={})
+	private Set<User> admins;
+
+
+	public BaseNode() {
+
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setAdmins(Set<User> admins) {
+		this.admins = admins;
+	}
+
+	public Set<User> getAdmins() {
+		return admins;
+	}
+}
