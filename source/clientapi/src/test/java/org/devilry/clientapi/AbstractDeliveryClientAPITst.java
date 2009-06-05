d2ddc8c2716627bbdd210f3758897bff04399f47
Bendik <bendikro@gmail.com>
2009-06-05 17:59:41 +0200

    Added tests for clientapi

---
+++ source/clientapi/src/test/java/org/devilry/clientapi/AbstractDeliveryClientAPITst.java
@@ -0,0 +1,56 @@
+package org.devilry.clientapi;
+
+
+import javax.naming.NamingException;
+
+import org.devilry.core.dao.AssignmentNodeImpl;
+import org.devilry.core.dao.CourseNodeImpl;
+import org.devilry.core.dao.NodeImpl;
+import org.devilry.core.dao.PeriodNodeImpl;
+import org.devilry.core.daointerfaces.AssignmentNodeRemote;
+import org.devilry.core.daointerfaces.CourseNodeRemote;
+import org.devilry.core.daointerfaces.NodeRemote;
+import org.devilry.core.daointerfaces.PeriodNodeRemote;
+import org.junit.Before;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class AbstractDeliveryClientAPITst extends AbstractNodeClientAPITst {
+	PeriodNodeRemote periodNode;
+	long periodId;
+	long periodId2;
+	
+	AssignmentNodeRemote assignmentNode;
+	long assignmentId;
+	long assignmentId2;
+	
+	
+	@Before
+	public void setUp() throws NamingException {
+		super.setUp();
+		// Course
+		CourseNodeRemote courseNode = getRemoteBean(CourseNodeImpl.class);
+		long inf1000Id = courseNode.create("inf1000", "Object oriented programming", matnatId);
+		
+		Calendar start = new GregorianCalendar(2009, 00, 01);
+		Calendar end = new GregorianCalendar(2009, 05, 15);
+		
+		// Period
+		periodNode = getRemoteBean(PeriodNodeImpl.class);
+		periodId = periodNode.create("spring09", "Spring 2009", start.getTime(), end.getTime(), inf1000Id);
+		periodId2 = periodNode.create("fall09", "Fall 2009", start.getTime(), end.getTime(), inf1000Id);
+		
+		Calendar deadline = new GregorianCalendar(2009, 05, 17);
+		
+		// Assignment
+		assignmentNode = getRemoteBean(AssignmentNodeImpl.class);
+		assignmentId = assignmentNode.create("Oblig1", "Obligatory assignment 1", deadline.getTime(), periodId);
+		
+		deadline = new GregorianCalendar(2009, 06, 17);
+		assignmentId2 = assignmentNode.create("Oblig2", "Obligatory assignment 2", deadline.getTime(), periodId);
+		
+		deadline = new GregorianCalendar(2009, 07, 17);
+		assignmentId2 = assignmentNode.create("Oblig1", "Obligatory assignment 1", deadline.getTime(), periodId2);
+	}		
+}
