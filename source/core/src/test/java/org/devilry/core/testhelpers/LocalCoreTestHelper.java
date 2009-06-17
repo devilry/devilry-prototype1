6e7c17ff9203ee80e135b3363dd7f90f456dd7b2
Bro <bro@bro-laptop.(none)>
2009-06-17 22:54:36 +0200

    Added methods getDeliveryCommon getDeliveryCandidateCommon getFileMetaCommon and getFileDataBlockCommon

--- source/core/src/test/java/org/devilry/core/testhelpers/LocalCoreTestHelper.java
+++ source/core/src/test/java/org/devilry/core/testhelpers/LocalCoreTestHelper.java
@@ -3,11 +3,19 @@ package org.devilry.core.testhelpers;
 import javax.naming.NamingException;
 import org.devilry.core.dao.AssignmentNodeImpl;
 import org.devilry.core.dao.CourseNodeImpl;
+import org.devilry.core.dao.DeliveryCandidateImpl;
+import org.devilry.core.dao.DeliveryImpl;
+import org.devilry.core.dao.FileDataBlockImpl;
+import org.devilry.core.dao.FileMetaImpl;
 import org.devilry.core.dao.NodeImpl;
 import org.devilry.core.dao.PeriodNodeImpl;
 import org.devilry.core.dao.UserImpl;
 import org.devilry.core.daointerfaces.AssignmentNodeCommon;
 import org.devilry.core.daointerfaces.CourseNodeCommon;
+import org.devilry.core.daointerfaces.DeliveryCandidateCommon;
+import org.devilry.core.daointerfaces.DeliveryCommon;
+import org.devilry.core.daointerfaces.FileDataBlockCommon;
+import org.devilry.core.daointerfaces.FileMetaCommon;
 import org.devilry.core.daointerfaces.NodeCommon;
 import org.devilry.core.daointerfaces.PeriodNodeCommon;
 import org.devilry.core.daointerfaces.UserCommon;
@@ -16,6 +24,10 @@ public class LocalCoreTestHelper extends CoreTestHelper {
 
 	public LocalCoreTestHelper(String username, String password) throws NamingException {
 		super(username, password);
+	}
+	
+	public UserCommon getUserCommon() throws NamingException {
+		return getLocalBean(UserImpl.class);
 	}
 
 	public NodeCommon getNodeCommon() throws NamingException {
@@ -34,8 +46,20 @@ public class LocalCoreTestHelper extends CoreTestHelper {
 		return getLocalBean(AssignmentNodeImpl.class);
 	}
 
-	public UserCommon getUserCommon() throws NamingException {
-		return getLocalBean(UserImpl.class);
+	public DeliveryCommon getDeliveryCommon() throws NamingException {
+		return getLocalBean(DeliveryImpl.class);
+	}
+
+	public DeliveryCandidateCommon getDeliveryCandidateCommon() throws NamingException {
+		return getLocalBean(DeliveryCandidateImpl.class);
+	}
+	
+	public FileMetaCommon getFileMetaCommon() throws NamingException {
+		return getLocalBean(FileMetaImpl.class);
+	}
+	
+	public FileDataBlockCommon getFileDataBlockCommon() throws NamingException {
+		return getLocalBean(FileDataBlockImpl.class);
 	}
 
 }
