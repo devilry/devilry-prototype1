9c6bc9991b2fc36c2e9eeac360717231cd8f3b00
Bro <bro@bro-laptop.(none)>
2009-06-19 00:00:48 +0200

    Must extend examiner test and not student

--- source/clientapi/src/test/java/org/devilry/clientapi/ExaminerAssignmentRemoteTest.java
+++ source/clientapi/src/test/java/org/devilry/clientapi/ExaminerAssignmentRemoteTest.java
@@ -4,7 +4,7 @@ import javax.naming.NamingException;
 import org.devilry.core.testhelpers.RemoteCoreTestHelper;
 import org.junit.BeforeClass;
 
-public class ExaminerAssignmentRemoteTest extends StudentAssignmentCommonTest {
+public class ExaminerAssignmentRemoteTest extends ExaminerAssignmentCommonTest {
 	@BeforeClass
 	public static void initBeans() throws NamingException {
 		connection = new DevilryConnectionOpenEJB("homer", "doh");
