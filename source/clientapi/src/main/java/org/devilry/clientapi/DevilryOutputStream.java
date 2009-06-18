e2cdcd9f6191507f6c70292449f5679a000ec7a7
Bro <bro@bro-laptop.(none)>
2009-06-18 14:13:05 +0200

    Throw Exception

--- source/clientapi/src/main/java/org/devilry/clientapi/DevilryOutputStream.java
+++ source/clientapi/src/main/java/org/devilry/clientapi/DevilryOutputStream.java
@@ -3,19 +3,20 @@ package org.devilry.clientapi;
 import javax.naming.NamingException;
 
 import org.devilry.core.daointerfaces.FileDataBlockCommon;
+import org.devilry.core.daointerfaces.FileMetaCommon;
 
 public class DevilryOutputStream extends AbstractDevilryFileStream {
 
 	FileDataBlockCommon fileDataBlock;
 	
-	DevilryOutputStream(long fileMetaId, DevilryConnection connection) {
+	DevilryOutputStream(long fileMetaId, DevilryConnection connection) throws NamingException {
 		super(fileMetaId, connection);
 	}
 
 	private FileDataBlockCommon getFileDataBlockBean() throws NamingException {
 		return fileDataBlock == null ? fileDataBlock = connection.getFileDataBlock() : fileDataBlock;
 	}
-	
+		
 	public void write(byte [] data) throws NamingException {
 		getFileDataBlockBean().create(fileMetaId, data);
 	}
