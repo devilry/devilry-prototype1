89eb104e4c157933f7466990953f42d72a4bd378
Bro <bro@bro-laptop.(none)>
2009-06-18 17:42:59 +0200

    Removed unused imprts

--- source/clientapi/src/test/java/org/devilry/clientapi/DevilryFileStreamCommonTest.java
+++ source/clientapi/src/test/java/org/devilry/clientapi/DevilryFileStreamCommonTest.java
@@ -4,8 +4,6 @@ import javax.naming.*;
 
 import org.devilry.core.InvalidNameException;
 import org.devilry.core.NoSuchObjectException;
-import org.devilry.core.NoSuchUserException;
-import org.devilry.core.NodePath;
 import org.devilry.core.PathExistsException;
 import org.devilry.core.UnauthorizedException;
 import org.devilry.core.daointerfaces.AssignmentNodeCommon;
@@ -16,20 +14,14 @@ import org.devilry.core.daointerfaces.FileMetaCommon;
 import org.devilry.core.daointerfaces.NodeCommon;
 import org.devilry.core.daointerfaces.PeriodNodeCommon;
 import org.devilry.core.daointerfaces.UserCommon;
-import org.devilry.core.testhelpers.CoreTestHelper;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collection;
 import java.util.GregorianCalendar;
-import java.util.Iterator;
-import java.util.List;
 
 public abstract class DevilryFileStreamCommonTest {
 		
