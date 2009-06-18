31f04325819473ca5f6f3e9b85c1bf576a04dd42
Bro <bro@bro-laptop.(none)>
2009-06-18 14:16:32 +0200

    Take a filepath as argument in addFile and create FileMeta before creating outputstream

--- source/clientapi/src/main/java/org/devilry/clientapi/StudentDeliveryCandidate.java
+++ source/clientapi/src/main/java/org/devilry/clientapi/StudentDeliveryCandidate.java
@@ -1,15 +1,27 @@
 package org.devilry.clientapi;
 
+import javax.naming.NamingException;
+
+import org.devilry.core.daointerfaces.FileMetaCommon;
+
 
 public class StudentDeliveryCandidate extends AbstractDeliveryCandidate {
+	
+	FileMetaCommon fileMeta;
 	
 	StudentDeliveryCandidate(long deliveryCandidateId, DevilryConnection connection) {
 		super(deliveryCandidateId, connection);
 	}
 	
+	protected FileMetaCommon getFileMetaBean() throws NamingException {
+		return fileMeta == null ? fileMeta = connection.getFileMeta() : fileMeta;
+	}
 	
-	public DevilryOutputStream addFile() {
-		DevilryOutputStream outputStream = new DevilryOutputStream(deliveryCandidateId, connection);
+	public DevilryOutputStream addFile(String filePath) throws NamingException {
+		
+		long fileMetaId = getFileMetaBean().create(deliveryCandidateId, filePath);
+		
+		DevilryOutputStream outputStream = new DevilryOutputStream(fileMetaId, connection);
 		return outputStream;
 	}
 	
