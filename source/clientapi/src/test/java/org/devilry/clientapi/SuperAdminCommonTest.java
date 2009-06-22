4f01b486373a54d874db22831950b7ffecd02ccd
Bro <bro@bro-laptop.(none)>
2009-06-23 00:40:08 +0200

    Added actual tests

--- source/clientapi/src/test/java/org/devilry/clientapi/SuperAdminCommonTest.java
+++ source/clientapi/src/test/java/org/devilry/clientapi/SuperAdminCommonTest.java
@@ -1,9 +1,12 @@
 package org.devilry.clientapi;
 
+import static org.junit.Assert.*;
+
 import javax.naming.*;
 
 import org.devilry.core.InvalidNameException;
 import org.devilry.core.NoSuchObjectException;
+import org.devilry.core.NodePath;
 import org.devilry.core.PathExistsException;
 import org.devilry.core.UnauthorizedException;
 import org.devilry.core.daointerfaces.CourseNodeCommon;
@@ -12,26 +15,29 @@ import org.devilry.core.daointerfaces.PeriodNodeCommon;
 import org.devilry.core.daointerfaces.UserCommon;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
 
 public abstract class SuperAdminCommonTest {
 		
 	protected static DevilryConnection connection;
-	
-	protected long uioId, matnatId, ifiId, inf1000, inf1000Spring09, inf1000Fall09;
-	
+		
 	protected long homerId;
 	protected long bartId;
 	protected long lisaId;
 	
-	protected NodeCommon node;
+	SuperAdmin superAdmin;
+	
+	NodeCommon node;
 	CourseNodeCommon courseNode;
 	PeriodNodeCommon periodNode;
 	
-	protected UserCommon userBean;
+	UserCommon userBean;
 
 	
 	ArrayList<String> names = new ArrayList<String>();
@@ -68,26 +74,13 @@ public abstract class SuperAdminCommonTest {
 		userBean.addIdentity(homerId, identity.get(0));
 		userBean.setIsSuperAdmin(homerId, true);
 		
+		superAdmin = new SuperAdmin(homerId, connection);
+		
 		bartId = userBean.create(names.get(1), emails.get(1), phoneNumbers.get(1));
 		userBean.addIdentity(bartId, identity.get(1));
 		
 		lisaId = userBean.create(names.get(2), emails.get(2), phoneNumbers.get(2));
 		userBean.addIdentity(lisaId, identity.get(2));
-		
-		
-		// Add nodes
-		uioId = node.create("uio", "UiO");
-		matnatId = node.create("matnat", "Faculty of Mathematics", uioId);
-		ifiId = node.create("ifi", "Department of Informatics", matnatId);
-						
-		inf1000 = courseNode.create("inf1000", "Programmering intro", ifiId);
-		
-		Calendar start = new GregorianCalendar(2009, 00, 01, 10, 15);
-		Calendar end = new GregorianCalendar(2009, 05, 15);
-		
-		inf1000Spring09 = periodNode.create("spring2009", "INF1000 spring2009", start.getTime(), end.getTime(), inf1000);
-		inf1000Fall09 = periodNode.create("fall2009", "INf1000 fall 2009", start.getTime(), end.getTime(), inf1000);
-		
 	}
 		
 
@@ -101,5 +94,86 @@ public abstract class SuperAdminCommonTest {
 		for(long userId: userBean.getUsers()) {
 			userBean.remove(userId);
 		}
+	}
+	
+	@Test
+	public void isSuperAdmin() throws NamingException {
+		assertTrue(superAdmin.isSuperAdmin(homerId));
+	}
+	
+	@Test
+	public void setIsSuperAdmin() throws NamingException {
+		superAdmin.setIsSuperAdmin(lisaId, true);
+		assertTrue(superAdmin.isSuperAdmin(lisaId));
+		
+		superAdmin.setIsSuperAdmin(lisaId, false);
+		assertFalse(superAdmin.isSuperAdmin(lisaId));
+	}
+	
+	@Test
+	public void addTopLevelNode() throws NamingException, PathExistsException, UnauthorizedException, InvalidNameException, NoSuchObjectException {
+		
+		AdminNode topLevelNode = superAdmin.addTopLevelNode("testTopLevel", "Just for testing top level nodes");
+				
+		long nodeId = node.getIdFromPath(new NodePath(new String[]{"testTopLevel"}));
+		assertEquals(topLevelNode.nodeId, nodeId);
+	}
+	
+	@Test(expected = NoSuchObjectException.class) 
+	public void removeTopLevelNode() throws NamingException, PathExistsException, UnauthorizedException, InvalidNameException, NoSuchObjectException {
+		
+		AdminNode topLevelNode = superAdmin.addTopLevelNode("testTopLevel", "Just for testing top level nodes");
+		superAdmin.removeTopLevelNode(topLevelNode);
+				
+		// Object does not exist and should produce NoSuchObjectException
+		long nodeId = node.getIdFromPath(new NodePath(new String[]{"testTopLevel"}));
+	}
+	
+	@Test
+	public void getTopLevelNodes() throws NamingException, PathExistsException, UnauthorizedException, InvalidNameException, NoSuchObjectException {
+		
+		AdminNode topLevelNode = superAdmin.addTopLevelNode("testTopLevel", "Just for testing top level nodes");
+		AdminNode topLevelNode2 = superAdmin.addTopLevelNode("testTopLevel2", "Just for testing top level nodes2");
+		
+		List<AdminNode> topLevelNodes = superAdmin.getTopLevelNodes();
+		
+		for (AdminNode node : topLevelNodes) {
+			assertTrue(node.nodeId == topLevelNode.nodeId || node.nodeId == topLevelNode2.nodeId);
+		}		
+	}
+	
+	@Test
+	public void addUser() throws NamingException, PathExistsException, UnauthorizedException, InvalidNameException, NoSuchObjectException {
+		
+		long userId = superAdmin.addUser("Test1", "Email2", "2342342342");
+		String test1Identity = "test1Ientity";
+		superAdmin.addIdentity(userId, test1Identity);
+		
+		assertEquals(userId, superAdmin.findUser(test1Identity));
+	}
+	
+	@Test
+	public void removeUser() throws NamingException, PathExistsException, UnauthorizedException, InvalidNameException, NoSuchObjectException {
+		
+		long userId = superAdmin.addUser("Test1", "Email2", "2342342342");
+		superAdmin.removeUser(userId);
+		
+		assertFalse(superAdmin.userExists(userId));
+	}
+		
+	@Test
+	public void userExists() throws NamingException, PathExistsException, UnauthorizedException, InvalidNameException, NoSuchObjectException {
+		long userId = superAdmin.addUser("Test1", "Email2", "2342342342");		
+		assertTrue(superAdmin.userExists(userId));
+	}
+	
+	
+	@Test
+	public void findUser() throws NamingException, PathExistsException, UnauthorizedException, InvalidNameException, NoSuchObjectException {
+		long userId = superAdmin.addUser("Test1", "Email2", "2342342342");
+		String test1Identity = "test1Ientity";
+		superAdmin.addIdentity(userId, test1Identity);
+		
+		assertEquals(userId, superAdmin.findUser(test1Identity));
 	}
 }
